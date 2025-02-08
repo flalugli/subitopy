8855bbf6c6993847ad7c1a53ef26eda6bbad141a
flalugli <flugli@mail.com>
2025-02-08 17:57:19 +0100

    modified docustring

--- src/subitopy/search_api.py
+++ src/subitopy/search_api.py
@@ -24,7 +24,8 @@ class Search:
         api_version : int, optional
             api version you want to use, by default 1
         proxy : str | None, optional
-            proxy, if passed all the requests will be made passing through this proxy , by default None
+            if passed all the requests will be made passing through this proxy.  
+            Format should be "http://username:password@host:port" , by default None
 
         """
 
